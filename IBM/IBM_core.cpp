1d880028f38abf796dec54f67b885a74a13abf1e
ramiadantsoa <tramiada@umn.edu>
2016-06-21 16:07:01 -0500

    there is a bug in patch size in section_colonization line 54
    -no sure what happens but the code works for small values and crashes after some time!!!

--- IBM/IBM_core.cpp
+++ IBM/IBM_core.cpp
@@ -61,11 +61,11 @@ boost::mt19937 rng(std::time(0));
 // typedef number<cpp_dec_float<100> > cpp_dec_float_100;
 
 int main(){
-	double z = 1.0, aggreg = 0.5, size = 6, simtime = 1000, muR = 0.1;
+	double z = 1.0, aggreg = 0.5., size = 12, simtime = 1000, muR = 0.1;
 	short int est = 1,  M = 1, replicates = 0, com_id = 1;
-	double lambda = 0.2, gammaH = 1.0;
+	double lambda = 0.4, gammaH = 1.0;
 
-  std::time_t seed_time = std::time(0);
+  std::time_t seed_time = 1; //std::time(0);
   rng.seed(seed_time);
 
 	// Parameter input
